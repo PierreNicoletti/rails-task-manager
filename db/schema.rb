46da7715332e4d13c51cb4c33fa1ca7139dd4865
Pierre Nicoletti <pierre.nicoletti@gmail.com>
2019-11-12 16:15:22 +0100

    add edit and delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_103628) do
+ActiveRecord::Schema.define(version: 2019_11_12_142951) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
